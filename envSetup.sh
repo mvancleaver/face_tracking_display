150de80918ac2899a8915220df4ed31671968b86
Matt VanCleave <35089445+mvancleaver@users.noreply.github.com>
2020-11-20 17:44:54 -0800

    debugging and added Realsense
    working thru envSetup to make sure it all works. some error with building rs-Python on jetson.

--- envSetup.sh
+++ envSetup.sh
@@ -1,37 +1,56 @@
 # General Clean-Up and Dependancies
 sudo rm -r ~/Documents ~/Music ~/Pictures ~/Public ~/Templates ~/Videos ~/examples.desktop
 sudo apt update && sudo apt install -y build-essential byobu i2c-tools git arp-scan nano git
+echo 'source ~/.bashrc' >> ~/.bash_profile
 
-# Python 2
-sudo apt install -y python python-pip python3 pyton3-pip
-pip install -y opencv-python future dlib smbus
-pip install -y --upgrade imutils
 
 # Python 3 + Circuit Python
-sudo apt install -y python3 pyton3-pip
-pip3 install -y opencv-python future dlib smbus
-sudo pip3 install adafruit-circuitpython-servokit adafruit-circuitpython-bno055
-pip3 install -y --upgrade imutils
-sudo apt update
-sudo apt upgrade
+sudo apt install -y python3 python3-pip python3-dev 
+pip3 install --upgrade pip
+pip3 install future dlib smbus numpy opencv-python
+pip3 install adafruit-circuitpython-servokit adafruit-circuitpython-bno055
+git clone https://github.com/jrosebr1/imutils.git
+cd imutils/
+sudo python3 setup.py install
+cd 
+sudo apt update && sudo apt upgrade
 sudo pip3 install --upgrade setuptools
 pip3 freeze - local | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U
 sudo groupadd -f -r gpio
 sudo usermod -a -G gpio user
-cd ~
+
 git clone https://github.com/NVIDIA/jetson-gpio.git
 sudo cp ~/jetson-gpio/lib/python/Jetson/GPIO/99-gpio.rules /etc/udev/rules.d
 
-# javascipt / node
-
-
+# Realsense2.0 SDK on Jetson
+mkdir lib_src && cd lib_src
+sudo apt-get install -y curl libssl-dev libcurl4-openssl-dev libxinerama-dev libxcursor-dev
+wget http://www.cmake.org/files/v3.13/cmake-3.13.0.tar.gz
+tar xpvf cmake-3.13.0.tar.gz cmake-3.13.0/
+cd cmake-3.13.0/
+./bootstrap --system-curl
+make -j4
+echo 'export PATH=/home/nvidia/cmake-3.13.0/bin/:$PATH' >> ~/.bashrc
+source ~/.bashrc
+cd ..
+wget https://github.com/IntelRealSense/librealsense/archive/v2.38.1.tar.gz
+tar -xf v2.38.1.tar.gz 
+cd librealsense-2.38.1/
+./scripts/setup_udev_rules.sh
+mkdir build && cd build
+cmake ../ -DBUILD_EXAMPLES=true -DBUILD_PYTHON_BINDINGS:bool=true -DBUILD_WITH_CUDA:bool=true -DBUILD_CV_EXAMPLES:bool=true -DCMAKE_CUDA_COMPILER:PATH=/usr/local/cuda/bin/nvcc -DPYTHON_EXECUTABLE=/usr/bin/python3
+make && sudo make install
+echo 'export PATH=/home/nvidia/cmake-3.13.0/bin/:$PATH' >> ~/.bashrc
+echo 'export PATH=/home/nvidia/cmake-3.13.0/bin/:$PATH' >> ~/.bashrc
+echo 'export PATH=/home/nvidia/cmake-3.13.0/bin/:$PATH' >> ~/.bashrc
+source ~/.bashrc
+cd ..
+# import pyrealsense2 as rs
+# rs.__path__ ['/usr/lib/python3/dist-packages/pyrealsense2']
+# sudo cp ~/librealsense/wrappers/python/pyrealsense2/__init__.py /usr/lib/python3/dist-packages/pyrealsense2/
 # Jetson Package Setups
 mkdir JetsonHacksSetup/ && cd JetsonHacksSetup/
-# Install Librealsense
-git clone https://github.com/JetsonHacksNano/installLibrealsense.git
-cd installLibrealsense
-./installLibrealsense.sh
-cd ..
+# Jetson VS Code
 git clone https://github.com/JetsonHacksNano/installVSCode.git
 cd installVSCode
 ./installVSCode.sh
@@ -39,7 +58,6 @@ cd
 
 mkdir exapmpleRepos && cd exapmpleRepos
 git clone https://github.com/antoinelame/GazeTracking.git
-pip install -r requirements.txt
-#git clone https://github.com/1adrianb/face-alignment.git
-pip3 install face-alignment	
+pip3 install -r requirements.txt
+git clone https://github.com/1adrianb/face-alignment.git
 cd ..
--- faceTrackingControl/gaze_tracking.py
+++ faceTrackingControl/gaze_tracking.py
@@ -8,89 +8,160 @@ import time
 import board
 import busio
 import json
-import adafruit_bno055
+import numpy as np
+
+######### IMU Driver Init 
+import adafruit_bno055 as imu
+i2c = busio.I2C(board.SCL, board.SDA) 
+feedback = imu.BNO055_I2C(i2c)
+
+######### PWM Board Init
 from adafruit_servokit import ServoKit
+i2c_1 = busio.I2C(board.SCL_1, board.SDA_1)  
+axis = ServoKit(channels=16, i2c=i2c_1)
+
+######### Realsense Depth Camera 
+import pyrealsense2 as rs
+# Create a pipeline
+pipeline = rs.pipeline() 
+#Create a config and configure the pipeline to stream
+#  different resolutions of color and depth streams
+config = rs.config()
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+# Start streaming
+profile = pipeline.start(config)
+# Getting the depth sensor's depth scale (see rs-align example for explanation)
+depth_sensor = profile.get_device().first_depth_sensor()
+depth_scale = depth_sensor.get_depth_scale()
+print("Depth Scale is: " , depth_scale)
+# We will be removing the background of objects more than
+#  clipping_distance_in_meters meters away
+clipping_distance_in_meters = 1 #1 meter
+clipping_distance = clipping_distance_in_meters / depth_scale
+# Create an align object
+# rs.align allows us to perform alignment of depth frames to others frames
+# The "align_to" is the stream type to which we plan to align depth frames.
+align_to = rs.stream.color
+align = rs.align(align_to)
+
+######### Gaze Init 
 from gaze_tracking import GazeTracking
-
-# IMU Init 
-i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_bno055.BNO055_I2C(i2c)
-x_home = sensor.euler[0]
-y_home = sensor.euler[1]
-z_home = sensor.euler[2]
-
-# PWM Driver Init 
-i2c_1 = busio.I2C(board.SCL_1, board.SDA_1)
-kit = ServoKit(channels=16, i2c=i2c_1)
-
-# Gaze Init 
 gaze = GazeTracking()
 webcam = cv2.VideoCapture(0)
 
 ############################################################################################################
-###################################################### - Control + Motion Primatives
+###################################################### - Motion Primatives + Controller 
 ############################################################################################################
 
+######### Axis Motion Primatives
+x_axis_control = axis.servo[0].angle
+x_home = feedback.euler[0]
+x_feedback = feedback.euler[0]
+x_setpoint = x_home
 
-# Axis Motion Primatives
+def home_X():
+    print("Homing X")
+
 def move_X():
     print("Moving X")
 
-def move_Y():
-    print("Moving Y")
 
-def move_Z():
-    print("Moving Z")
-
-# Homing 
-def home_X():
-    print("Homing X")
+y_axis_control = axis.servo[1].angle
+y_home = feedback.euler[1]
+y_feedback = feedback.euler[1]
+y_setpoint = y_home
 
 def home_Y():
     print("Homing Y")
 
+def move_Y():
+    print("Moving Y")
+
+
+z_axis_control = axis.servo[2].angle
+z_home = feedback.euler[2]
+z_feedback = feedback.euler[2]
+z_setpoint = z_home
+
+def move_Z():
+    print("Moving Z")
+
 def home_Z():
     print("Homing Z")
 
+
 def home_All():
-    print("Homing X")
-    print("Homing Y")
-    print("Homing Z")
+    home_X()
+    home_Y()
+    home_Z()
+
 
 ############################################################################################################
 ###################################################### - System Main 
 ############################################################################################################
 
-while True:
+try:
+    while True:
+        # Get frameset of color and depth
+        frames = pipeline.wait_for_frames()
+        # frames.get_depth_frame() is a 640x360 depth image
+
+        # Align the depth frame to color frame
+        aligned_frames = align.process(frames)
+
+        # Get aligned frames
+        aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+        color_frame = aligned_frames.get_color_frame()
+
+        # Validate that both frames are valid
+        if not aligned_depth_frame or not color_frame:
+            continue
+
+        depth_image = np.asanyarray(aligned_depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+
+        # Remove background - Set pixels further than clipping_distance to grey
+        grey_color = 153
+        depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
+        bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
    
-# Grab a frame and process Gaze info 
-    _, frame = webcam.read()
-    gaze.refresh(frame)
-    new_frame = gaze.annotated_frame()
-    text = ""
-    if gaze.is_right():
-        text = "Looking right"
-        move_X()
-    elif gaze.is_left():
-        text = "Looking left"
-        move_X()
-    elif gaze.is_center():
-        text = "Looking center"
-        home_ALL()
+        # Grab a color frame and process Gaze info 
+        gaze.refresh(color_image)
+        new_frame = gaze.annotated_frame()
+        text = ""
+        if gaze.is_right():
+            text = "Looking right"
+            move_X()
+        elif gaze.is_left():
+            text = "Looking left"
+            move_X()
+        elif gaze.is_center():
+            text = "Looking center"
+            home_ALL()
 
-    cv2.putText(new_frame, text, (60, 60), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, 0), 2)
-    cv2.imshow("Demo", new_frame)
-    if cv2.waitKey(1) == 27:
-        break
+        cv2.putText(new_frame, text, (60, 60), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, 0), 2)
+        cv2.imshow("Demo", new_frame)
+        if cv2.waitKey(1) == 27:
+            break
 
-# Print Data to Terminal / JSON
-    print("==========================================")
-    print("Euler angle X: {}".format(sensor.euler[0]))
-    print("Euler angle Y: {}".format(sensor.euler[1]))
-    print("Euler angle Z: {}".format(sensor.euler[2]))
-    print()
-    data = {"eurler":{"X":sensor.euler[0],"Y":sensor.euler[1],"Z":sensor.euler[2]}}
-    with open("data_file.json", "w") as write_file:
-        json.dump(data, write_file)
+         # Render images
+        #depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+        #images = np.hstack((bg_removed, depth_colormap))
+        #cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
+        #cv2.imshow('Align Example', images)
+        #key = cv2.waitKey(1)
+        # Press esc or 'q' to close the image window
+        #if key & 0xFF == ord('q') or key == 27:
+        #    cv2.destroyAllWindows()
+        #    break
+        # Print IMU Data to JSON
+        #print("Euler angle X: {}".format(feedback.euler[0]))
+        #print("Euler angle Y: {}".format(feedback.euler[1]))
+        #print("Euler angle Z: {}".format(feedback.euler[2]))
 
-    time.sleep(0.035) # Set to loop at ~30FPS
+        data = {"eurler":{"X":feedback.euler[0],"Y":feedback.euler[1],"Z":feedback.euler[2]}}
+        with open("data_file.json", "w") as write_file:
+            json.dump(data, write_file)
+finally:
+    pipeline.stop()
